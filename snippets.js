a8f03735f6be690b553cfe3a1195ed2701c3b104
Armienn <nigathr@gmail.com>
2025-10-23 22:00:44 +0200

    Add beginning of Danmarks Krønike

---
+++ snippets.js
@@ -0,0 +1,37 @@
+// Saxos Danmarks Krønike fra grundtvigsværker.dk
+var metaElements = [ ]
+do {
+  metaElements = [
+    ...$0.getElementsByClassName("pbA"),
+    ...$0.getElementsByClassName("rs_bible"),
+    ...$0.getElementsByClassName("segStart"),
+    ...$0.getElementsByClassName("segEnd"),
+    ...$0.getElementsByClassName("lgNumber"),
+  ]
+  for (const element of metaElements)
+    element.remove()
+}
+while (metaElements.length > 0)
+
+//var tekst = $0.textContent
+
+//var tekst = "<p>" + $0.textContent.split("    ").join("</p><p>") + "</p>"
+
+for (const element of [...$0.getElementsByClassName("firstIndent"), ...$0.getElementsByClassName("noIndent")])
+  element.innerHTML = "¤p(" + element.innerHTML + "¤p)"
+for (const element of $0.getElementsByTagName("table"))
+  element.innerHTML = "¤pre(" + element.innerHTML + "¤pre)"
+for (const element of $0.getElementsByClassName("l_firstIndent"))
+  element.innerHTML = element.innerHTML + "¤"
+for (const element of $0.getElementsByClassName("center"))
+  element.innerHTML = "¤div(" + element.innerHTML + "¤div)"
+var tekst = $0.textContent
+console.log(tekst
+  .split("¤p(").join("<p>")
+  .split("¤p)").join("</p>")
+  .split("¤pre(").join("<pre>")
+  .split("¤pre)").join("</pre>")
+  .split("¤div(").join("<div>")
+  .split("¤div)").join("</div>")
+  .split("¤").join("\n")
+)
