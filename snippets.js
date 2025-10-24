042b1705ba030cc6611138ca60407f8bb7dfd509
Armienn <nigathr@gmail.com>
2025-10-24 16:19:24 +0200

    Add fourth book to Danmarks Krønike

--- snippets.js
+++ snippets.js
@@ -19,25 +19,29 @@ while (metaElements.length > 0)
 //var tekst = "<p>" + $0.textContent.split("    ").join("</p><p>") + "</p>"
 
 for (const element of [...$0.getElementsByClassName("firstIndent"), ...$0.getElementsByClassName("noIndent")])
-  element.innerHTML = "¤p(" + element.innerHTML + "¤p)"
+  element.innerHTML = "¤p|(" + element.innerHTML + "¤p|)"
 for (const element of $0.getElementsByTagName("table"))
-  element.innerHTML = "¤pre(" + element.innerHTML + "¤pre)"
+  element.innerHTML = "¤pre|(" + element.innerHTML + "¤pre|)"
 for (const element of $0.getElementsByClassName("l_firstIndent"))
   element.innerHTML = element.innerHTML + "¤"
 for (const element of $0.getElementsByClassName("l_secondIndent"))
   element.innerHTML = "¤|" + element.innerHTML + "¤"
 for (const element of $0.getElementsByClassName("l_refrain"))
   element.innerHTML = element.innerHTML + "¤"
+for (const element of $0.getElementsByClassName("italic"))
+  element.innerHTML = "¤em|(" + element.innerHTML + "¤em|)"
 for (const element of $0.getElementsByClassName("center"))
-  element.innerHTML = "¤div(" + element.innerHTML + "¤div)"
+  element.innerHTML = "¤div|(" + element.innerHTML + "¤div|)"
 var tekst = $0.textContent
 console.log(tekst
-  .split("¤p(").join("<p>")
-  .split("¤p)").join("</p>")
-  .split("¤pre(").join("<pre>")
-  .split("¤pre)").join("</pre>")
-  .split("¤div(").join("<div>")
-  .split("¤div)").join("</div>")
+  .split("¤p|(").join("<p>")
+  .split("¤p|)").join("</p>")
+  .split("¤pre|(").join("<pre>")
+  .split("¤pre|)").join("</pre>")
+  .split("¤div|(").join("<div>")
+  .split("¤div|)").join("</div>")
+  .split("¤em|(").join("<em>")
+  .split("¤em|)").join("</em>")
   .split("¤|").join("\t")
   .split("¤").join("\n")
 )
